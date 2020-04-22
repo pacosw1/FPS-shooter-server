6c15ae15307c01a8247cd64febcc103e383f1b1b
Paco <pacoswilliams@gmail.com>
2020-04-21 22:48:08 -0500

    lowering fps

--- simulation/physics.go
+++ simulation/physics.go
@@ -74,7 +74,7 @@ func (e *Engine) Broadcast(s *state.GameState) {
 
 //GameLoop tick
 func (e *Engine) GameLoop(t <-chan time.Time) {
-	br := time.Duration(1000 / 15)
+	br := time.Duration(1000 / 10)
 	tick := time.Tick(br * time.Millisecond)
 	for e.State == 1 {
 		select {
