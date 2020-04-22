ff2b786594bf0fab684fabb9c203e9d150cdd1e0
Paco <pacoswilliams@gmail.com>
2020-04-21 19:26:21 -0500

    why

--- entity/entity.go
+++ entity/entity.go
@@ -29,7 +29,7 @@ func (p *Player) UpdatePlayer(r *message.NetworkInput) {
 	speed := 3
 	p.SequenceID = uint16(r.SequenceID) //check overload of uint 16
 	p.IsShooting = r.IsShooting
-
+	//
 	//update player position and facing vector (rotation)
 	p.UpdateRotation(r.Rotation.X, r.Rotation.Y)
 	p.UpdateMovement(r.Direction, speed)
